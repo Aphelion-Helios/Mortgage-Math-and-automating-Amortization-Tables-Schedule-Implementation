acc63f91b31819ae96d0e9a7f87393b1d5966031
Ramsey Hammad <hammadrkh@gmail.com>
2024-12-14 21:21:31 -0500

    Developing a simple Mortgage estimation App with VBA Userforms
    Developing a simple Mortgage estimation App with VBA Userforms

---
+++ Mortgage Math and constructing Amortization Tables (VBA code).vb
@@ -0,0 +1,15 @@
+Private Sub CommandButton1_Click()
+
+Unload UserForm1
+
+End Sub
+
+
+
+Private Sub CommandButton2_Click()
+
+SQRB = (1 - (1 + r / m) ^ (-T * m)) / (r / m)
+
+MP = Principal / SQRB
+
+End Sub
